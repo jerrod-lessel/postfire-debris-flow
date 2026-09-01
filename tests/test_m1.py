41e311a13da64f79316435176742777195f6d0f5
Jerrod Lessel <jerrod.lessel@gmail.com>
2026-09-01 14:28:28 -0700

    Update test_m1.py

--- tests/test_m1.py
+++ tests/test_m1.py
@@ -1,7 +1,7 @@
 import math
 import pytest
 
-from pfdf.m1 import (
+from debrisflow.m1 import (
     BasinPredictors, RainfallConvention, likelihood, hazard_class,
     rainfall_threshold, to_model_rainfall,
 )
